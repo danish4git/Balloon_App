ac76a45c138b10fbe6a137ee093349acdbe249a4
DANISH AHMED <danish4eng@gmail.com>
2022-05-28 17:57:43 +0530

    added move functionality

--- src/components/Baloon.jsx
+++ src/components/Baloon.jsx
@@ -9,70 +9,103 @@ const Baloon = () => {
   const [cir3, setCir3] = useState("block");
   const [cir4, setCir4] = useState("block");
   const [cir5, setCir5] = useState("block");
-
+  const [cir11, setCir11] = useState("none");
+  const [cir22, setCir22] = useState("none");
+  const [cir33, setCir33] = useState("none");
+  const [cir44, setCir44] = useState("none");
+  const [cir55, setCir55] = useState("none");
+  //////right div////////
   const Circlediv1 = styled.div`
     background-color: #d5e8d4;
     border: 2px solid #c1d4bb;
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
     display: ${cir1};
   `;
   const Circlediv2 = styled.div`
     background-color: #ffe6cd;
     border: 2px solid #c9b378;
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
     display: ${cir2};
   `;
   const Circlediv3 = styled.div`
     background-color: #6a00ff;
     border: 2px solid #804dc6;
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
     display: ${cir3};
   `;
   const Circlediv4 = styled.div`
     background-color: #d9e8fb;
     border: 2px solid #b0bdcd;
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
     display: ${cir4};
   `;
   const Circlediv5 = styled.div`
     background-color: #e2d5e7;
     border: 2px solid #c9bccd;
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
     display: ${cir5};
   `;
 
+  //////left div////////
+  const Circlediv11 = styled.div`
+    background-color: #d5e8d4;
+    border: 2px solid #c1d4bb;
+    display: ${cir11};
+  `;
+  const Circlediv22 = styled.div`
+    background-color: #ffe6cd;
+    border: 2px solid #c9b378;
+    display: ${cir22};
+  `;
+  const Circlediv33 = styled.div`
+    background-color: #6a00ff;
+    border: 2px solid #804dc6;
+    display: ${cir33};
+  `;
+  const Circlediv44 = styled.div`
+    background-color: #d9e8fb;
+    border: 2px solid #b0bdcd;
+    display: ${cir44};
+  `;
+  const Circlediv55 = styled.div`
+    background-color: #e2d5e7;
+    border: 2px solid #c9bccd;
+    display: ${cir55};
+  `;
+
   const inputHandler = () => {
-    switch (input) {
-      case 1:
-        setCir1("none");
+    console.log(input);
 
-        break;
-      case 2:
-        setCir2("none");
+    if (input == 1) {
+      setCir1("none");
+      setCir11("block");
+    } else if (input == 2) {
+      setCir2("none");
+      setCir22("block");
+    } else if (input == 3) {
+      setCir3("none");
+      setCir33("block");
+    } else if (input == 4) {
+      setCir4("none");
+      setCir44("block");
+    } else if (input == 5) {
+      setCir5("none");
+      setCir55("block");
+    }
+  };
 
-        break;
-      case 3:
-        setCir3("none");
-
-        break;
-      case 4:
-        setCir4("none");
-
-        break;
-      case 5:
-        setCir5("none");
-
-        break;
+  const reversehandler = (value) => {
+    console.log("hi");
+    if (value == 1) {
+      setCir11("none");
+      setCir1("block");
+    } else if (value == 2) {
+      setCir22("none");
+      setCir2("block");
+    } else if (value == 3) {
+      setCir33("none");
+      setCir3("block");
+    } else if (value == 4) {
+      setCir44("none");
+      setCir4("block");
+    } else if (value == 5) {
+      setCir55("none");
+      setCir5("block");
     }
   };
 
@@ -80,16 +113,16 @@ const Baloon = () => {
     <div>
       <div className="inputdiv">
         <input type="number" onChange={(e) => setInput(e.target.value)} />
-        <button onClick={inputHandler()}>Shoot</button>
+        <button onClick={() => inputHandler()}>Shoot</button>
       </div>
       <h1>{input}</h1>
       <div className="container">
         <div className="boxdiv">
-          <div className="circle1">1</div>
-          <div className="circle2">2</div>
-          <div className="circle3">3</div>
-          <div className="circle4">4</div>
-          <div className="circle5">5</div>
+          <Circlediv11 onClick={() => reversehandler(1)}>1</Circlediv11>
+          <Circlediv22 onClick={() => reversehandler(2)}>2</Circlediv22>
+          <Circlediv33 onClick={() => reversehandler(3)}>3</Circlediv33>
+          <Circlediv44 onClick={() => reversehandler(4)}>4</Circlediv44>
+          <Circlediv55 onClick={() => reversehandler(5)}>5</Circlediv55>
         </div>
         <div className="circle_main_div">
           <Circlediv1>1</Circlediv1>
