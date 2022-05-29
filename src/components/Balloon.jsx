68d4fd124edd52e0a094558a65ab7e7de17b4e65
DANISH AHMED <danish4eng@gmail.com>
2022-05-29 13:50:15 +0530

    added border based on balloons colors

--- src/components/Balloon.jsx
+++ src/components/Balloon.jsx
@@ -3,56 +3,69 @@ import "./balloon.css";
 import styled from "styled-components";
 
 const Balloon = () => {
-  let colors = ["#d5e8d4", "#ffe6cd", "#6a00ff", "#d9e8fb", "#e2d5e7"];
-  let randomColor = colors.sort((a, b) => 0.5 - Math.random());
+  let colorindex = [0, 1, 2, 3, 4];
+  let randomColor = colorindex.sort((a, b) => 0.5 - Math.random());
   const [input, setInput] = useState(0);
   const [cir1, setCir1] = useState(true);
   const [cir2, setCir2] = useState(true);
   const [cir3, setCir3] = useState(true);
   const [cir4, setCir4] = useState(true);
   const [cir5, setCir5] = useState(true);
-  const [colorsArray, setcolorsArray] = useState(randomColor);
+  const [colorind, setColornd] = useState(randomColor);
+
+  let colorsArray = ["#d5e8d4", "#ffe6cd", "#6a00ff", "#d9e8fb", "#e2d5e7"];
+  let borderColor = ["#c1d4bb", "#c9b378", "#804dc6", "#b0bdcd", "#c9bccd"];
 
   const Circlediv1 = styled.div`
-    background-color: ${colorsArray[0]};
+    background-color: ${colorsArray[colorind[0]]};
+    border: 3px solid ${borderColor[colorind[0]]};
     display: ${cir1 ? "block" : "none"};
   `;
   const Circlediv2 = styled.div`
-    background-color: ${colorsArray[1]};
+    background-color: ${colorsArray[colorind[1]]};
+    border: 3px solid ${borderColor[colorind[1]]};
     display: ${cir2 ? "block" : "none"};
   `;
   const Circlediv3 = styled.div`
-    background-color: ${colorsArray[2]};
+    background-color: ${colorsArray[colorind[2]]};
+    border: 3px solid ${borderColor[colorind[2]]};
     display: ${cir3 ? "block" : "none"};
   `;
   const Circlediv4 = styled.div`
-    background-color: ${colorsArray[3]};
+    background-color: ${colorsArray[colorind[3]]};
+    border: 3px solid ${borderColor[colorind[3]]};
     display: ${cir4 ? "block" : "none"};
   `;
   const Circlediv5 = styled.div`
-    background-color: ${colorsArray[4]};
+    background-color: ${colorsArray[colorind[4]]};
+    border: 3px solid ${borderColor[colorind[4]]};
     display: ${cir5 ? "block" : "none"};
   `;
 
   //////left div////////
   const Circlediv11 = styled.div`
-    background-color: ${colorsArray[0]};
+    background-color: ${colorsArray[colorind[0]]};
+    border: 3px solid ${borderColor[colorind[0]]};
     display: ${cir1 ? "none" : "block"};
   `;
   const Circlediv22 = styled.div`
-    background-color: ${colorsArray[1]};
+    background-color: ${colorsArray[colorind[1]]};
+    border: 3px solid ${borderColor[colorind[1]]};
     display: ${cir2 ? "none" : "block"};
   `;
   const Circlediv33 = styled.div`
-    background-color: ${colorsArray[2]};
+    background-color: ${colorsArray[colorind[2]]};
+    border: 3px solid ${borderColor[colorind[2]]};
     display: ${cir3 ? "none" : "block"};
   `;
   const Circlediv44 = styled.div`
-    background-color: ${colorsArray[3]};
+    background-color: ${colorsArray[colorind[3]]};
+    border: 3px solid ${borderColor[colorind[3]]};
     display: ${cir4 ? "none" : "block"};
   `;
   const Circlediv55 = styled.div`
-    background-color: ${colorsArray[4]};
+    background-color: ${colorsArray[colorind[4]]};
+    border: 3px solid ${borderColor[colorind[4]]};
     display: ${cir5 ? "none" : "block"};
   `;
 
